92a8c876a7114491803f7f531b939175c341848c
james wafula <jameswafula@jamess-MacBook-Pro-2.local>
2019-07-15 15:54:45 +0300

    chore(db): Setup database
    - create a db connection
    
    - add environment variable
    
    [Delivers #167285011]

---
+++ graphql/schema/index.js
@@ -0,0 +1,27 @@
+// Schema goes here
+
+// const { buildSchema } = require('grapql');
+
+// const graphqlSchema = buildSchema(`
+
+//     type user{
+//         userName: String!
+//         email: String!
+//         password: String!
+//     }
+
+//     type blog{
+//         image: File!
+//         caption: String!
+//     }
+//     type rootQuery{}
+//     type rootMutation{}
+
+
+//     schema{
+//         query: rootQuery
+//         mutation: rootMutation
+//     }
+// `)
+
+// module.exports = graphqlSchema;
