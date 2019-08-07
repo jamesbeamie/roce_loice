6cfd737a50939d2b20453bf463108460a60415a7
jamesbeamie <wafulajames9@gmail.com>
2019-08-07 23:29:26 +0300

    Remove fb auth

--- server.js
+++ server.js
@@ -3,8 +3,8 @@ const bodyParser = require('body-parser');
 const expressGraphQL = require('express-graphql');
 const mongoose = require('mongoose');
 const { apolloUploadExpress } = require('apollo-upload-server');
-const passport = require('passport');
-const FacebookStrategy = require('passport-facebook');
+// const passport = require('passport');
+// const FacebookStrategy = require('passport-facebook');
 // const expresshandlebars = require('express-handlebars');
 // const nodemailer = require('nodemailer');
 
@@ -31,27 +31,27 @@ app.use(
   }),
 );
 
-app.use(passport.initialize());
+// app.use(passport.initialize());
 
-passport.use(
-  new FacebookStrategy(
-    {
-      clientID: process.env.FB_CLIENT_ID,
-      clientSecret: process.env.FB_CLIENT_SECRET,
-      callbackURL: process.env.FB_CALLBACK_URL,
-    },
-    (accessToken, refreshToken, profile, cb) => {
-      console.log(profile);
-      cb(null, profile);
-    },
-  ),
-);
+// passport.use(
+//   new FacebookStrategy(
+//     {
+//       clientID: process.env.FB_CLIENT_ID,
+//       clientSecret: process.env.FB_CLIENT_SECRET,
+//       callbackURL: process.env.FB_CALLBACK_URL,
+//     },
+//     (accessToken, refreshToken, profile, cb) => {
+//       console.log(profile);
+//       cb(null, profile);
+//     },
+//   ),
+// );
 
-app.get('/fblogin', passport.authenticate('facebook'));
+// app.get('/fblogin', passport.authenticate('facebook'));
 
-app.get('/auth/facebook/callback', passport.authenticate('facebook', { session: false }), (req, res) => {
-  res.send('authenticated');
-});
+// app.get('/auth/facebook/callback', passport.authenticate('facebook', { session: false }), (req, res) => {
+//   res.send('authenticated');
+// });
 
 // database connection
 mongoose
@@ -62,7 +62,6 @@ mongoose
   )
   .then(() => {
     app.listen(process.env.PORT || 8080);
-    console.log('connected to server');
   })
   .catch((err) => {
     throw err;
