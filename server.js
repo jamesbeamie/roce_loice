695823e1492837e15b5b817a50e4610734393f77
jamesbeamie <wafulajames9@gmail.com>
2019-08-13 20:05:04 +0300

    implement cors

--- server.js
+++ server.js
@@ -19,6 +19,16 @@ app.use(bodyParser.json());
 app.use(apolloUploadExpress({ uploadDir: './ ' }));
 // app.post('/upload', upload.fields([{ name: 'file' }]));
 
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'POST, GET, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  if (req.method === 'OPTIONS') {
+    return res.sendStatus(200);
+  }
+  next();
+});
+
 // authentication middleware
 app.use(isAuth);
 
